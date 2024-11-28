e3d0ded8f886692641ecb615ebc300eaed71f18b
Kate <01194270@pw.edu.pl>
2024-11-28 04:02:53 +0300

    added prototype of notes page

---
+++ registration-login-system/src/main/java/net/javaguides/springboot/repository/NoteRepository.java
@@ -0,0 +1,10 @@
+package net.javaguides.springboot.repository;
+
+import net.javaguides.springboot.entity.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findNoteByUserId(Long id);
+}
---
+++ registration-login-system/src/main/java/net/javaguides/springboot/service/NoteService.java
@@ -0,0 +1,19 @@
+package net.javaguides.springboot.service;
+
+import net.javaguides.springboot.dto.NoteDto;
+
+import java.util.List;
+
+public interface NoteService {
+    void addNote(NoteDto noteDto);
+
+    NoteDto getNoteById(Long id);
+
+    List<NoteDto> getAllNotes();
+
+    List<NoteDto> getAllNotesByUser();
+
+    NoteDto updateNote(NoteDto noteDto, Long id);
+
+    void deleteNoteById(Long id);
+}
