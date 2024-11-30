772288735606abb2888040a3ebae3dbfa59c1108
Kate <01194270@pw.edu.pl>
2024-12-01 01:22:08 +0300

    added main logic to notes

--- registration-login-system/src/main/java/net/javaguides/springboot/repository/NoteRepository.java
+++ registration-login-system/src/main/java/net/javaguides/springboot/repository/NoteRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
-    List<Note> findNoteByUserId(Long id);
+    List<Note> findNoteByUserId(Long userId);
 }
--- registration-login-system/src/main/java/net/javaguides/springboot/service/impl/NoteServiceImpl.java
+++ registration-login-system/src/main/java/net/javaguides/springboot/service/impl/NoteServiceImpl.java
@@ -1,8 +1,9 @@
 package net.javaguides.springboot.service.impl;
 
+import lombok.AllArgsConstructor;
+import net.javaguides.springboot.CurrentUser;
 import net.javaguides.springboot.dto.NoteDto;
 import net.javaguides.springboot.entity.Note;
-import net.javaguides.springboot.entity.User;
 import net.javaguides.springboot.repository.NoteRepository;
 import net.javaguides.springboot.service.NoteService;
 import org.springframework.stereotype.Service;
@@ -11,54 +12,46 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 public class NoteServiceImpl implements NoteService {
 
-    private final NoteRepository noteRepository;
-
-    public NoteServiceImpl(NoteRepository noteRepository) {
-        this.noteRepository = noteRepository;
-    }
+    private NoteRepository noteRepository;
 
     @Override
     public void addNote(NoteDto noteDto) {
-        Note note = new Note();
-        note.setTitle(noteDto.getTitle());
-        note.setContent(noteDto.getContent());
-        note.setUser(getCurrentUser()); // Реализуйте метод получения текущего пользователя
+        Note note = mapToNote(noteDto);
+        note.setUser(CurrentUser.user);
         noteRepository.save(note);
     }
 
     @Override
     public NoteDto getNoteById(Long id) {
         Note note = noteRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Note not found"));
-        return convertToDto(note);
+                .orElseThrow(() -> new RuntimeException("Note not found with id: " + id));
+        return mapToNoteDto(note);
     }
 
     @Override
     public List<NoteDto> getAllNotes() {
-        return noteRepository.findAll().stream()
-                .map(this::convertToDto)
-                .collect(Collectors.toList());
+        List<Note> notes = noteRepository.findAll();
+        return notes.stream().map(this::mapToNoteDto).collect(Collectors.toList());
     }
 
     @Override
     public List<NoteDto> getAllNotesByUser() {
-        Long userId = getCurrentUserId(); // Реализуйте метод получения текущего userId
-        return noteRepository.findNoteByUserId(userId).stream()
-                .map(this::convertToDto)
-                .collect(Collectors.toList());
+        List<Note> notes = noteRepository.findNoteByUserId(CurrentUser.user.getId());
+        return notes.stream().map(this::mapToNoteDto).collect(Collectors.toList());
     }
 
     @Override
     public NoteDto updateNote(NoteDto noteDto, Long id) {
         Note note = noteRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Note not found"));
-
+                .orElseThrow(() -> new RuntimeException("Note not found with id: " + id));
         note.setTitle(noteDto.getTitle());
         note.setContent(noteDto.getContent());
-        noteRepository.save(note);
-        return convertToDto(note);
+
+        Note updatedNote = noteRepository.save(note);
+        return mapToNoteDto(updatedNote);
     }
 
     @Override
@@ -66,17 +59,11 @@ public class NoteServiceImpl implements NoteService {
         noteRepository.deleteById(id);
     }
 
-    private NoteDto convertToDto(Note note) {
+    private NoteDto mapToNoteDto(Note note) {
         return new NoteDto(note.getId(), note.getTitle(), note.getContent());
     }
 
-    private Long getCurrentUserId() {
-        // TODO
-        return 1L;
-    }
-
-    private User getCurrentUser() {
-        // TODO
-        return new User();
+    private Note mapToNote(NoteDto noteDto) {
+        return new Note(noteDto.getId(), noteDto.getTitle(), noteDto.getContent(), CurrentUser.user);
     }
 }
