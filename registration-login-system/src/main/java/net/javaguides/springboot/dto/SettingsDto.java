f8b38e81dba513aac42cf05f5715201980428a0e
dpelin <01194266@pw.edu.pl>
2024-12-04 15:57:52 +0100

    Added settings page (backend)

---
+++ registration-login-system/src/main/java/net/javaguides/springboot/dto/SettingsDto.java
@@ -0,0 +1,18 @@
+package net.javaguides.springboot.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SettingsDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+}
+
